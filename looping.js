5601132fd61ae4e9b1d242344eb1a7db7ac9eadb
Fema19 <fachrinurulakbar19@gmail.com>
2025-07-31 12:07:02 +0700

    tugas 5

---
+++ looping.js
@@ -0,0 +1,13 @@
+console.log("Bilangan Ganjil:");
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+console.log("\nBilangan Genap:");
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
